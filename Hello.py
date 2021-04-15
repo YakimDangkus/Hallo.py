86fa3f6b7c11fec24f70d8d17245536ef36ea5ed
YakimDangkus <82529911+YakimDangkus@users.noreply.github.com>
2021-04-15 20:54:17 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+Python v3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Nama: Yoyakim Dangkus")
+Nama: Yoyakim Dangkus
+>>> print("Kelas: A")
+Kelas: A
+>>> print("Kampus: Universitas Narotama")
+Kampus: Universitas Narotama
+>>> 
